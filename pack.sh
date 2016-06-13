af6cd53a95cdd1d5e4cc7d39e27ea7c11cae86a9
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2016-06-13 16:52:04 +0200

    Made pack.sh better
    - make ffmpeg quiet
    - switch by region
    - clean up

--- pack.sh
+++ pack.sh
@@ -15,6 +15,25 @@ if [[ -z "$(which ffmpeg)" ]]; then
   exit 0
 fi
 
+if [[ $# -ne 1 ]]; then
+    echo "Illegal number of parameters"
+fi
+
+#FIXME: all regions
+region=""
+
+case $1 in
+    "-E")
+        region="EUR"
+        ;;
+    "-U")
+        region="USA"
+        ;;
+    *)
+        echo "This is not supported"
+        exit 0
+esac
+
 costummakerom="makerom -target t"
 
 themes="`ls Themes`"
@@ -24,7 +43,7 @@ do
   #FIXME: first check if such an icon.* exists
   if test -n "$(find Themes/$theme -maxdepth 1 -name 'icon.*' -print -quit)"; then
     convert Themes/$theme/icon.* -resize 48x48 -background black -gravity center -extent 48x48 icons/$theme.png
-    ffmpeg -vcodec png -i icons/$theme.png -vcodec rawvideo -f rawvideo -pix_fmt rgb565 icons/$theme.icn
+    ffmpeg -hide_banner -loglevel panic -vcodec png -i icons/$theme.png -vcodec rawvideo -f rawvideo -pix_fmt rgb565 icons/$theme.icn
   else
     #FIXME: handle default icon
     echo "Do stuff with default icon"
@@ -38,3 +57,9 @@ done
 #FIXME: generate final cia
 
 #FIXME: clean up
+if test -n "$(find icons -maxdepth 1 -name '*' -print -quit)"; then
+  rm icons/*
+fi
+if test -n "$(find . -maxdepth 1 -name '*.cfa' -print -quit)"; then
+  rm *.cfa
+fi
