f5871e65e5364788fa9f991560eff0c4027c10d5
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2016-06-13 21:19:09 +0200

    First step towards a working name injection

--- pack.sh
+++ pack.sh
@@ -4,17 +4,14 @@ if [[ -z "$(which makerom)" ]]; then
   echo "Make sure makerom is in" '$PATH'
   exit 0
 fi
-
 if [[ -z "$(which convert)" ]]; then
   echo "imagemagic is a dependecy install it!!!"
   exit 0
 fi
-
 if [[ -z "$(which ffmpeg)" ]]; then
   echo "ffmpeg is a dependecy install it!!!"
   exit 0
 fi
-
 if [[ $# -ne 1 ]]; then
     echo "Illegal number of parameters"
 fi
@@ -29,6 +26,9 @@ case $1 in
     "-U")
         region="USA"
         ;;
+    "-J")
+	region="JAP"
+	;;
     *)
         echo "This is not supported"
         exit 0
@@ -37,13 +37,23 @@ esac
 costummakerom="makerom -target t"
 
 themes="`ls Themes`"
+themesshortend=("${themes[@]}")
+numthemes=${#themes[@]}
 
-for theme in ${themes[@]}
+#FIXME: a bin works as follows input is $themesshortend[${i}]
+# dd conv=notrunc of=tools\${region}\rom\ContentInfoArchive_${region}_${lang}.bin bs=1 seek=$((8+${i}*200))
+
+for (( i=0; i<${numthemes}; i++ ));
+do
+  themesshortend[i] = $(echo $themes[i]|cut -c1-64)
+done
+
+for (( i=0; i<${numthemes}; i++ ));
 do
   #FIXME: first check if such an icon.* exists
-  if test -n "$(find Themes/$theme -maxdepth 1 -name 'icon.*' -print -quit)"; then
-    convert Themes/$theme/icon.* -resize 48x48 -background black -gravity center -extent 48x48 icons/$theme.png
-    ffmpeg -hide_banner -loglevel panic -vcodec png -i icons/$theme.png -vcodec rawvideo -f rawvideo -pix_fmt rgb565 icons/$theme.icn
+  if test -n "$(find Themes/$themes[i] -maxdepth 1 -name 'icon.*' -print -quit)"; then
+    convert Themes/$themes[i]/icon.* -resize 48x48 -background black -gravity center -extent 48x48 icons/$i.png
+    ffmpeg -hide_banner -loglevel panic -vcodec png -i icons/$i.png -vcodec rawvideo -f rawvideo -pix_fmt rgb565 icons/$i.icn
   else
     #FIXME: handle default icon
     echo "Do stuff with default icon"
