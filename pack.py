cce007de2301b27b9a9dd41751ae4e98bf3df44e
Copper Phosphate <cu3po42@gmail.com>
2016-06-14 16:41:05 +0200

    Extract icon from info.smdh and copy theme files to tmp directory

--- pack.py
+++ pack.py
@@ -11,8 +11,8 @@ from distutils.spawn import find_executable
 ThemeEntry = namedtuple('ThemeEntry', ['foldername', 'uid'])
 
 tmpDir = TemporaryDirectory()
-iconDir = os.path.join(tmpDir.name, 'icons')
-os.makedirs(iconDir)
+normalizedThemes = os.path.join(tmpDir, 'themes')
+os.makedirs(normalizedThemes)
 
 def assertToolsExist():
     assert find_executable('makerom'), 'Install makerom and ensure it is in $PATH'
@@ -25,20 +25,27 @@ def parseCmd():
     regionGroup.add_argument('-e', action='append_const', dest='region', const='e')
     regionGroup.add_argument('-u', action='append_const', dest='region', const='u')
     regionGroup.add_argument('-j', action='append_const', dest='region', const='j')
-    regoinGroup.add_argument('-r', '--reverse' action='store_false', help='Sort the themes so they show up in reverse alphabetical order in the 3DS')
+    parser.add_argument('-r', '--reverse' action='store_false', help='Sort the themes so they show up in reverse alphabetical order in the 3DS')
     return parser.parse_args()
 
-def convertIcon(theme):
-    themeIcon = os.path.join(theme.foldername, 'icon')
-    tmpIcon = os.path.join(iconDir, theme.uid)
+def getIcon(source, dest):
+    themeIcon = os.path.join(source, 'icon')
+    tmpIcon = os.path.join(dest, 'icon')
+    infoSmdh = os.path.join(source, 'info.smdh')
     if os.path.exists(themeIcon + '.icn'):
         copyfile(themeIcon + '.icn', tmpIcon + '.icn')
     else if [x for x in next(os.walk(theme.foldername))[2] if x.startswith('icon.')]:
         subprocess.call(['convert', themeIcon + '.*', '-resize', '48x48', '-background', 'black', '-gravity' 'center', '-extent', '48x48', tmpIcon + '.png'], shell=True)
         subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'panic', '-vcodec', 'png', '-i', tmpIcon + '.png', '-vcodec', 'rawvideo', '-pix_fmt', 'rgb56565', tmpIcon + '.icn'])
         os.remove(tmpIcon + '.png')
+    else if os.path.exists(infoSmdh):
+        with open(infoSmdh, 'rb') as f:
+            f.seek(0x24C0)
+            icon = f.read(0x1200)
+            with open(tmpIcon + '.icn', 'w') as iconFile:
+                f.write(icon)
     else:
-        # TODO parse info.smdh or use default
+        # TODO use default
         raise Error('Not implemented yet')
 
 def strToFixedUtf8(string, length):
@@ -51,18 +58,38 @@ def strToFixedUtf8(string, length):
             enc = enc[:-1]
     return enc + ((length - len(enc)) * b'\x00')
 
-def isThemeComplete(theme: ThemeEntry):
-    files = next(os.walk(theme.foldername))[2]
+def isThemeComplete(folder):
+    files = next(os.walk(folder))[2]
     return 'body_LZ.bin' in files and 'info.smdh' in files
 
-def listThemes():
+def processThemes():
     thisDir = os.path.dirname(__file__)
     themeDir = os.path.join(thisDir, 'themes')
     folders = next(os.walk(themeDir))[1]
     i = 0
     for folder in folders:
-        yield createThemeEntry(folder, i)
+        if not isThemeComplete():
+            continue
+        yield processTheme(folder, i)
         i += 1
 
-def createThemeEntry(folder, uid):
-    raise Error('Not implemented yet')
+def tryCopy(source, dest):
+    if os.path.exists(source):
+        copyfile(source, dest)
+
+def processTheme(folder, uid):
+    themeDir = os.path.join(normalizedThemes, uid)
+    os.makedirs(themeDir)
+    tryCopy(os.path.join(folder, 'body_LZ.bin'), themeDir)
+    tryCopy(os.path.join(folder, 'info.smdh'), themeDir) # TODO figure out if we need this file
+    tryCopy(os.path.join(folder, 'bgm.bcstm'), themeDir)
+    getIcon(folder, themeDir)
+
+    return ThemeEntry(folder, uid)
+
+def doConversion(region, reverse):
+    raise Error('not implemented yet')
+
+if __name__ == '__main__':
+    args = parse_args()
+    doConversion(args.region[0], args.reverse)
